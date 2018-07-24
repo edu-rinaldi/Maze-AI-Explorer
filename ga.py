6e66e49b547039fb5b0c6522672617220447156d
edu-rinaldi <eduardo.rinaldi1998@gmail.com>
2018-07-24 08:38:52 +0200

    created genetic algorithm
    (to be changed)

--- ga.py
+++ ga.py
@@ -1,43 +1,95 @@
 import random
-
+from Maze import Maze
+import immagini
 
 class GAPath:
-    def __init__(self, initX, initY):
-        randx, randy = initX+random.randrange(-1,1), initY+1+random.randrange(-1,1)
-        self.path = [(initX,initY), (initX, initY+1)]
-        if (randx,randy) not in self.path:
-            self.path.append((randx,randy))
-        self.fitness = -1
+    #(x,y)
+    offset = [(0,1),(0,-1),(1,0),(-1,0)]
+    def __init__(self, initX, initY, endX, endY):
+        self.fitness = 0
+        self.path = [(initX, initY)]
+        self.penalita = 0
+        self.end = (endX, endY)
     def __str__(self):
-        return "Path coords:\t"+str(self.path)+"\nFitness value:\t"+str(self.fitness)
+        return "Fitness:\t"+str(self.fitness)+"\tCur coords:\t"+str(self.getCurrentCoords())+"\tPenalita':\t"+str(self.penalita)
+
+    def getCurrentCoords(self):
+        return self.path[-1]
+
+    def distance(self):
+        return abs(self.getCurrentCoords()[0] - self.end[0]) + abs(self.getCurrentCoords()[1] - self.end[1])
+
+def initPopulation(n,init,end):
+    """initialize population with starting and ending points"""
+    return [GAPath(init[0],init[1],end[0],end[1]) for _ in range(n)]
 
 
-def initPopulation(sizePopulation, initX, initY):
-    return [GAPath(initX, initY) for _ in range(sizePopulation)]
-
-
-def checkValidCoords(img,x,y):
-    return img[y][x] == (255,255,255)
-
-
-def distance(sx,sy,ex,ey):
-    return abs(sx-ex) + abs(sy-ey)
-
-def fitness(img, population, endX, endY):
+def fitness(population, maze):
+    """"""
+    width, height = len(maze[0]), len(maze)
     for element in population:
-        coords = element.path[-1]
-        element.fitness = -1 if not checkValidCoords(img, coords[0], coords[1]) else distance(coords[0],coords[1], endX, endY)
+        x,y = random.choice(GAPath.offset)
+        cx, cy = element.getCurrentCoords()
+        if not (0<=cx+x<width and 0<=cy+y<height) or maze[cy+y][cx+x] == (0,0,0) or (cx+x,cy+y) in element.path:
+            element.penalita -= 10
+        else:
+            element.path.append((cx+x, cy+y))
+
+        for ox, oy in GAPath.offset:
+            cx, cy = element.getCurrentCoords()
+            f = False
+            if (0<=cx+ox<width and 0<=cy+oy<height) and maze[cy+oy][cx+ox] == (255,255,255) and not (cx+ox,cy+oy) in element.path:
+                element.penalita += 20
+                f = True
+            element.penalita -= 10 if not f else 0
+
+        element.fitness = element.distance() + element.penalita
     return population
 
+def selection(population, npop):
+    population.sort(key=lambda x: x.fitness, reverse=True)
 
-def ga(img, sizePopulation, gens, initX, initY, endX, endY):
-    population = initPopulation(sizePopulation, initX, initY)
+    population = population[:int(npop*0.2)]
+    return population
+
+def crossover(population, npop, init, end):
+    for _ in range(npop-len(population)):
+        parent = random.choice(population)
+        parent2 = random.choice(population)
+
+        child = GAPath(init[0],init[1],end[0],end[1])
+        child.path = [x for x in parent.path if x in parent2.path]
+        # if len(parent.path)>1:
+        #     child.path = parent.path[:int(len(parent.path)*random.uniform(0.5,1))]
+        population.append(child)
+    return population
+
+def mutation(population):
+    for element in population:
+        if len(element.path)>1 and random.random()<= 0.002:
+            randomIndex = int(len(element.path)*random.uniform(0.3,1))
+            element.path = element.path[:randomIndex]
+    return population
+
+def ga(npop,gens,maze,init,end):
+
+    population = initPopulation(npop,init,end)
     for gen in range(gens):
-        population = fitness(img, population, endX,endY)
-        for pop in population:
-            print(pop)
+        #calcolo fitness value
+        population = fitness(population, maze)
+        #selezione
+        population = selection(population, npop)
+        #l'accoppiamento
+        population = crossover(population, npop, init, end)
+        #mutazione
+        population = mutation(population)
 
+    population.sort(key=lambda x: x.fitness,reverse=True)
+    return population
 
-ga(0,10,1,10,0,20,20)
+# maze = Maze(100,100)
+# while len(maze.frontier)>0:
+#     maze.workOneStep()
+# immagini.save(maze.getMaze(),"maze.png")
 
-
+#ga(20,20000,maze.getMaze(),(1,1),(98,98))
--- gui.py
+++ gui.py
@@ -1,18 +1,19 @@
 import pygame, png, immagini
 from Maze import Maze
-
-
+import ga
 class MainWindow:
     width = 100
     height = 100
-    blockSize = 5
+    blockSize = 10
     title = ""
     display = None
     pixelArray = None
+    maze = None
     def __init__(self, title, width, height):
         self.title  = title
-        self.width = width*self.blockSize
-        self.height = height*self.blockSize
+        self.maze = Maze(width, height)
+        self.width = self.maze.width*self.blockSize
+        self.height = self.maze.height*self.blockSize
         self.display = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(title)
         self.pixelArray = pygame.PixelArray(self.display)
@@ -26,23 +27,44 @@ class MainWindow:
             for x in range(sx, sx+width):
                 self._drawPixel(x,y, color)
         pygame.display.update()
-
+    def genMaze(self):
+        genMaze = True
+        while genMaze:
+            pygame.time.Clock()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    genMaze = False
+                    break
+            for y, x in self.maze.getToDraw():
+                self.drawRectangle(x * self.blockSize, y * self.blockSize, self.blockSize, self.blockSize,
+                                     self.maze.getColor(y, x))
+            if len(self.maze.frontier) == 0:
+                genMaze = False
+                break
+            pygame.display.update()
+            self.maze.workOneStep()
 if __name__=='__main__':
     pygame.init()
-    maze = Maze(200, 200)
+    window = MainWindow('labirinto', 50,50)
+    window.genMaze()
 
-    window = MainWindow('labirinto', maze.width, maze.height)
+    finalPopulation = ga.ga(200, 50000, window.maze.getMaze(), (1, 1), (48, 48))
+    finalPopulation.sort(key=lambda x: x.fitness)
+    coords = finalPopulation[0].path
     run = True
     while run:
-        pygame.time.Clock()
+        pygame.time.Clock().tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        for y,x in maze.getToDraw():
-            window.drawRectangle(y*window.blockSize, x*window.blockSize, window.blockSize, window.blockSize, maze.getColor(y, x))
-        pygame.display.update()
-        maze.workOneStep()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    """QUI VA GESTITO IL RESET CON IL TASTO R"""
 
+        if len(coords)>0:
+            x,y = coords.pop(0)
+            window.drawRectangle(x*window.blockSize, y*window.blockSize, window.blockSize, window.blockSize, (255,0,0))
+        pygame.display.update()
 
     pygame.quit()
