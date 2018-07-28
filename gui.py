f7b7688d2ac5cc7f675903032c14bf5ca5f560ef
Eduardo <eduardo.rinaldi1998@gmail.com>
2018-07-28 17:22:23 +0200

    removed useless try catch

--- gui.py
+++ gui.py
@@ -50,52 +50,47 @@ class MainWindow:
 """
     to change configuration, change config.py
 """
-try:
+if __name__=='__main__':
 
-    if __name__=='__main__':
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-        os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pygame.init()
+    window = MainWindow(cf.WINDOW_TITLE, cf.MAZE_WIDTH, cf.MAZE_HEIGHT, cf.BLOCK_SIZE)
+    window.genMaze()
 
-        pygame.init()
-        window = MainWindow(cf.WINDOW_TITLE, cf.MAZE_WIDTH, cf.MAZE_HEIGHT, cf.BLOCK_SIZE)
-        window.genMaze()
+    ga = GA(cf.GENERATIONS, cf.POPULATION_SIZE, cf.MUTATION_RATE, cf.START_COORDS, cf.END_COORDS, window.maze)
 
-        ga = GA(cf.GENERATIONS, cf.POPULATION_SIZE, cf.MUTATION_RATE, cf.START_COORDS, cf.END_COORDS, window.maze)
+    run = True
 
-        run = True
+    while run and ga.curgen<ga.gen and not ga.victory:
+        pygame.time.Clock()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        print("Current generation:" ,ga.curgen)
+        ga.nextGen()
+        toReset = set()
+        for p in ga.population:
+            for y,x in p.path:
+                toReset.add((y,x))
+                window.drawRectangle(x*window.blockSize, y*window.blockSize, window.blockSize, window.blockSize, p.color)
+        for y,x in toReset:
+            window.drawRectangle(x * window.blockSize, y * window.blockSize, window.blockSize, window.blockSize,
+                                 (255,255,255))
+        pygame.time.delay(500)
+        pygame.display.update()
+    print("Best path found", ga.bestPlayer)
+    run = True
+    while run:
+        pygame.time.Clock()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        for y,x in ga.bestPlayer.path:
+            pygame.time.Clock().tick(30)
+            window.drawRectangle(x * window.blockSize, y * window.blockSize, window.blockSize, window.blockSize,
+                                 p.color)
 
-        while run and ga.curgen<ga.gen and not ga.victory:
-            pygame.time.Clock()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    break
-            print("Current generation:" ,ga.curgen)
-            ga.nextGen()
-            toReset = set()
-            for p in ga.population:
-                for y,x in p.path:
-                    toReset.add((y,x))
-                    window.drawRectangle(x*window.blockSize, y*window.blockSize, window.blockSize, window.blockSize, p.color)
-            for y,x in toReset:
-                window.drawRectangle(x * window.blockSize, y * window.blockSize, window.blockSize, window.blockSize,
-                                     (255,255,255))
-            pygame.time.delay(500)
-            pygame.display.update()
-        print("Best path found", ga.bestPlayer)
-        run = True
-        while run:
-            pygame.time.Clock()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    break
-            for y,x in ga.bestPlayer.path:
-                pygame.time.Clock().tick(30)
-                window.drawRectangle(x * window.blockSize, y * window.blockSize, window.blockSize, window.blockSize,
-                                     p.color)
-
-        pygame.quit()
-except:
-    print("a")
-    input()
+    pygame.quit()
